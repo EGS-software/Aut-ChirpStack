1c4841d177d852b8871c34c9e1f421080012864b
JaoVicy <filipimjoa@gmail.com>
2025-08-18 18:47:31 -0300

    chore: added driver

--- driver_test.py
+++ driver_test.py
@@ -2,3 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+
+driver = webdriver.Safari()
+driver.get("https://www.google.com")
+
