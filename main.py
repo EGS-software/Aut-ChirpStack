e8dbcdac476dd2e72a7fd9a8c836f90f5b0b37b4
JaoVicy <filipimjoa@gmail.com>
2025-08-19 17:25:52 -0300

    feat: created def to route

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+
+def open_chirpstack():
+    driver = webdriver.Safari()
