8c5c538eedcf86da536a5acc331d0bbdab7b7548
JaoVicy <filipimjoa@gmail.com>
2025-08-22 13:37:03 -0300

    feat: added error import

--- windows/main.py
+++ windows/main.py
@@ -3,6 +3,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager # type: ignore
 import time
 
