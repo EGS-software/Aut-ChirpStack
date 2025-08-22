a10ff235ca18dbe336e5ae676dce45b9e25d314c
JaoVicy <filipimjoa@gmail.com>
2025-08-22 13:29:32 -0300

    feat: added sleep(5)

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ def open_chirpstack():
 
     event_button = driver.find_element(By.XPATH, "//a[@href='#/tenants/cde402c9-1549-4de5-b37c-b32755f5a70c/applications/e38b1e6a-599e-4a2d-9de3-695ea5b57877/devices/08a60f4954ee1820']")
     event_button.click()
+    time.sleep(5)
 
 
 
