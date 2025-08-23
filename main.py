8c989ea9457cfae3297c2799e888a6a3c3ac704a
JaoVicy <filipimjoa@gmail.com>
2025-08-23 10:34:28 -0300

    chore: added import

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 from dotenv import load_dotenv
+import os
 
 load_dotenv()  # Load environment variables from .env file
 
