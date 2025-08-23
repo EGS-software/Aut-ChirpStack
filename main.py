0b38724e9e737294317e94b2e01de5e47bdd5734
JaoVicy <filipimjoa@gmail.com>
2025-08-23 10:33:01 -0300

    chore: added import

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+from dotenv import load_dotenv
 
 def open_chirpstack():
     driver = webdriver.Safari()
